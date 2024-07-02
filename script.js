183cab9c375b1313373788f01a0139dad84882dd
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2024-07-03 01:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+const solve = (arr, n) => {
+	const obj = {};
+	for(let i=0;i<n;i++){
+		if(obj[arr[i]]){
+			obj[arr[i]]++;
+		}
+		else{
+			obj[arr[i]] = 1;
+		}
+	}
+	for(let i in obj){
+		if(obj.i === n/2){
+			return i;
+		}
+		return -1;
+	}
+}
